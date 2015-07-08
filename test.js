c26ffa0ecf5ca0bb34c92a0b43ebf80b3e9ec951
thirdcoder <boris3rd@bk.ru>
2015-07-07 23:09:29 -0700

    Add unary functions for 5-trit trytes

--- test.js
+++ test.js
@@ -1,3 +1,19 @@
 'use strict';
 
 var test = require('tape');
+var tritwise = require('./');
+var bts2n = require('balanced-ternary').bts2n;
+var n2bts = require('balanced-ternary').n2bts;
+var STI_5 = tritwise.STI_5;
+var FD_5= tritwise.FD_5;
+
+test('simple ternary inverter', function(t) {
+  t.equal(STI_5(bts2n('1100i')), bts2n('ii001'));
+  t.equal(STI_5(bts2n('000i1')), bts2n('0001i'));
+  t.end();
+});
+
+test('forward diode', function(t) {
+  t.equal(FD_5(bts2n('1100i')), bts2n('11000'));
+  t.end();
+});
--- tritwise.js
+++ tritwise.js
@@ -25,7 +25,29 @@ function unary_tritwise_op(tt, input, input_width) {
   return output;
 }
 
+var UNARY_TRUTH_TABLES = {
+  NTI: 5,   // 1ii negative ternary inverter
+  STI: 8,   // 10i simple ternary inverter
+  PTI: 11,  // 11i positive ternary inverter
+  FD: 1,    // 001 forward diode
+  RD: -9,   // i00 reverse diode
+};
+
+var TRYTE_SIZE = 5;
+
+function NTI_5(x) { return unary_tritwise_op(UNARY_TRUTH_TABLES.NTI, x, TRYTE_SIZE); }
+function STI_5(x) { return unary_tritwise_op(UNARY_TRUTH_TABLES.STI, x, TRYTE_SIZE); }
+function PTI_5(x) { return unary_tritwise_op(UNARY_TRUTH_TABLES.PTI, x, TRYTE_SIZE); }
+function FD_5(x)  { return unary_tritwise_op(UNARY_TRUTH_TABLES.FD,  x, TRYTE_SIZE); }
+function RD_5(x)  { return unary_tritwise_op(UNARY_TRUTH_TABLES.RD,  x, TRYTE_SIZE); }
+
 // STI
-console.log(pad(5, n2bts(unary_tritwise_op(bts2n('10i'), bts2n('0011i'), 5)), '0'));
+//console.log(pad(5, n2bts(unary_tritwise_op(bts2n('10i'), bts2n('0011i'), 5)), '0'));
 
-
+module.exports = {
+  NTI_5: NTI_5,
+  STI_5: STI_5,
+  PTI_5: PTI_5,
+  FD_5: FD_5,
+  RD_5: RD_5,
+};
