9dc2514908f356316cb532b4a20665d6af3123c6
thirdcoder <boris3rd@bk.ru>
2015-07-07 23:38:35 -0700

    Test positive ternary inverter, since it inverts 0 to 1 hence depends on tryte size

--- test.js
+++ test.js
@@ -4,7 +4,9 @@ var test = require('tape');
 var tritwise = require('./');
 var bts2n = require('balanced-ternary').bts2n;
 var n2bts = require('balanced-ternary').n2bts;
+
 var STI = tritwise.STI;
+var PTI = tritwise.PTI;
 var FD = tritwise.FD;
 var TOR = tritwise.TOR;
 var TAND = tritwise.TAND;
@@ -13,6 +15,11 @@ var BUT = tritwise.BUT;
 test('simple ternary inverter', function(t) {
   t.equal(STI(bts2n('1100i')), bts2n('ii001'));
   t.equal(STI(bts2n('000i1')), bts2n('0001i'));
+  t.end();
+});
+
+test('positive ternary inverter', function(t) {
+  t.equal(PTI(bts2n('00i01')), bts2n('1111i'));
   t.end();
 });
 
