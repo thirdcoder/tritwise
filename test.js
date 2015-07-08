40fe0eeceda97f42a70948d9c4051b1b4aa23d8e
thirdcoder <boris3rd@bk.ru>
2015-07-07 23:30:45 -0700

    Implement ternary OR, aka maximum, via preference functions

--- test.js
+++ test.js
@@ -5,7 +5,8 @@ var tritwise = require('./');
 var bts2n = require('balanced-ternary').bts2n;
 var n2bts = require('balanced-ternary').n2bts;
 var STI_5 = tritwise.STI_5;
-var FD_5= tritwise.FD_5;
+var FD_5 = tritwise.FD_5;
+var TOR_5 = tritwise.TOR_5;
 
 test('simple ternary inverter', function(t) {
   t.equal(STI_5(bts2n('1100i')), bts2n('ii001'));
@@ -15,5 +16,13 @@ test('simple ternary inverter', function(t) {
 
 test('forward diode', function(t) {
   t.equal(FD_5(bts2n('1100i')), bts2n('11000'));
+  t.end();
+});
+
+test('dyadic ternary or/maximum', function(t) {
+  t.equal(TOR_5(bts2n('101ii'),
+                bts2n('ii01i')),
+                bts2n('1011i'));
+
   t.end();
 });
--- tritwise.js
+++ tritwise.js
@@ -5,6 +5,7 @@ var n2bts = require('balanced-ternary').n2bts;
 var BT_DIGIT_TO_N = require('balanced-ternary').BT_DIGIT_TO_N;
 var pad = require('pad');
 
+// Unary operations
 function unary_tritwise_op(tt, input, input_width) {
   if (tt < -13 || tt > 13) throw new Error('unary_tritwise_op('+tt+'): out of range iii-111');
   var bt_tt = pad(3, n2bts(tt), '0');
@@ -41,8 +42,44 @@ function PTI_5(x) { return unary_tritwise_op(UNARY_TRUTH_TABLES.PTI, x, TRYTE_SI
 function FD_5(x)  { return unary_tritwise_op(UNARY_TRUTH_TABLES.FD,  x, TRYTE_SIZE); }
 function RD_5(x)  { return unary_tritwise_op(UNARY_TRUTH_TABLES.RD,  x, TRYTE_SIZE); }
 
-// STI
-//console.log(pad(5, n2bts(unary_tritwise_op(bts2n('10i'), bts2n('0011i'), 5)), '0'));
+
+// Dyadic preference functions
+function dyadic_pref_op(pref, input1, input2, input_width) {
+  if (pref < -13 || pref > 13) throw new Error('dyadic_pref_op('+pref+'): out of range iii-111');
+  var bt_pref = pad(3, n2bts(pref), '0');
+  if (bt_pref.indexOf('i') === -1 || bt_pref.indexOf('0') === -1 || bt_pref.indexOf('1') === -1)
+    throw new Error('dyadic_pref_op('+pref+'): preference must contain all digits i,0,1');
+
+  var input1_bt = pad(input_width, n2bts(input1), '0');
+  var input2_bt = pad(input_width, n2bts(input2), '0');
+
+  var output_bt = '';
+
+  for (var i = 0; i < input_width; ++i) {
+    var a = input1_bt.charAt(i);
+    var b = input2_bt.charAt(i);
+
+    if (bt_pref.indexOf(a) < bt_pref.indexOf(b)) {
+      // a has a higher preference than b (comes earlier in preference list), choose a
+      output_bt += a;
+    } else {
+      // otherwise choose b (preference functions always choose one of the inputs)
+      output_bt += b;
+    }
+  }
+
+  var output = bts2n(output_bt);
+
+  return output;
+}
+
+var DYADIC_PREFS = {
+  OR: 8,   // pref-10i, maximum
+  AND: -8, // pref-i01, minimum
+  BUT: -2, // pref-0i1
+};
+
+function TOR_5(a, b) { return dyadic_pref_op(DYADIC_PREFS.OR, a, b, TRYTE_SIZE); }
 
 module.exports = {
   NTI_5: NTI_5,
@@ -50,4 +87,6 @@ module.exports = {
   PTI_5: PTI_5,
   FD_5: FD_5,
   RD_5: RD_5,
+
+  TOR_5: TOR_5,
 };
